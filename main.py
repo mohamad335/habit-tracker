1eed72e6e2a04888aeaa414568972f91aa54aa62
Mohammad <mhamdl2amen@gmail.com>
2024-06-21 16:57:23 +0300

    i add graph end point

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import requests
 from datetime import datetime
-GRAPHID= "graph9"
-TOKEN = "wlndfsedrgk09"
-USERNAME = "boodi"
+GRAPHID= ""
+TOKEN = ""
+USERNAME = ""
 today = datetime.now()
 pixela_endpoint = " https://pixe.la/v1/users"
 user_params = {
@@ -11,3 +11,24 @@ user_params = {
     "agreeTermsOfService": "yes",
     "notMinor": "yes"
 }
+graph_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs"
+graph_config = {
+    "id": "graph9",
+    "name": "Cycling Graph",
+    "unit": "Km",
+    "type": "float",
+    "color": "ajisai"}
+headers = {
+    "X-USER-TOKEN": TOKEN
+}
+
+pixel_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs/{GRAPHID}"
+
+
+post_config = {
+    "date": today.strftime("%Y%m%d"),
+    "quantity": input("How many kilometers did you cycle today?"),
+
+}
+response = requests.post(url=pixel_endpoint, json=post_config, headers=headers)
+print(response.text)
